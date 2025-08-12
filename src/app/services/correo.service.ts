8e474dbbc2e22705c02ef7ddf7992493e06ed1be
Thegeka13 <rafaelcamachohdz1303@gmail.com>
2025-08-12 13:29:11 -0600

    Se grego el formulario para enviar correos desde la web y la pestaña de comentarios ahora se comparte con los usuarios

---
+++ src/app/services/correo.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Correo} from '../interfaces/correo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CorreoService {
+  private apiUrl = `${environment.apiUrl}Email`; // La URL base del controlador de correo
+
+  constructor(private http: HttpClient) {}
+
+  // Nuevo método para enviar correo de cotización
+  enviarCorreoCotizacion(datosCorreo: Correo): Observable<any> {
+    const url = `${this.apiUrl}/enviarCorreoCotizacion`;
+    return this.http.post<any>(url, datosCorreo);
+  }
+}
